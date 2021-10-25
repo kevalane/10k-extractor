cb06aa46c4ae871b1c23779a6d0f56d834e48c70
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-10-25 20:38:59 +0200

    Tested pdf reader. Working.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log("Hello World");
+// Imports
+const fs = require('fs'),
+PDFParser = require("pdf2json");
+
+const pdfParser = new PDFParser();
+
+pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+pdfParser.on("pdfParser_dataReady", pdfData => {
+    console.log(JSON.stringify(pdfData));
+    fs.writeFile("test.json", JSON.stringify(pdfData));
+});
+
+pdfParser.loadPDF("test.pdf");
+
 
