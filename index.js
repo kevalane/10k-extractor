6c9068fd4f9d901fd99cbe78be41f6aefcf74e05
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-10-26 18:45:59 +0200

    Found ways to loop over relevant information. Pivoting to express app to visualize json easier.

--- index.js
+++ index.js
@@ -7,8 +7,21 @@ const pdfParser = new PDFParser();
 pdfParser.on('pdfParser_dataError', err => console.error(err.parserError));
 pdfParser.on('pdfParser_dataReady', data => {
     // console.log(data);
-    console.log(data['Pages'][0]['Texts'][0]['R'][0]['T']);
+    // console.log(data['Pages'][0]['Texts'][0]['R'][0]['T']);
     // fs.writeFile("test.json", JSON.stringify(pdfData));
+    // for (let i = 0; i < data['Pages'].length; i++) {
+    //     console.log(i)
+    //     console.log(data['Pages'][i]['Texts'].length);
+    //     console.log(data['Pages'][i]['Texts'][0]['R'][0]['T']);
+        // for (let k = 0; k < data['Pages'][i]['Texts'].length; k++) {
+        //     if (i == data['Pages'].length) {
+        //         console.log(k);
+        //     }
+        // }
+    // }
+    fs.writeFile('test.json', data, () => {
+
+    });
 });
 
 pdfParser.loadPDF('test.pdf');
