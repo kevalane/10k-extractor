9fcd78c9b9b60d8e5f7988755fe47872c4d01deb
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-10-26 19:42:35 +0200

    Finalized one working loop to find selected financial data.

--- index.js
+++ index.js
@@ -9,7 +9,31 @@ app.listen(3000, () => {
 })
 
 app.get('/test', (req, res) => {
-    res.status(200).send({msg: 'Hello'});
+    // pdfParser.loadPDF('test.pdf');
+    let rawData = fs.readFileSync('test.json');
+    let data = JSON.parse(rawData);
+
+    let loops = 0;
+    let magicIndex = [0,0,0];
+    // Let's loop for 'selected%20financial%20data, occurence 2'
+    for (let i = 0; i < data['Pages'].length; i++) {
+        for (let k = 0; k < data['Pages'][i]['Texts'].length; k++) {
+            for (let m = 0; m < data['Pages'][i]['Texts'][k]['R'].length; m++) {
+                if (data['Pages'][i]['Texts'][k]['R'][m]['T'].toLowerCase().includes('selected%20financial%20data')) {
+                    loops++;
+                    if (loops == 2) {
+                        console.log(data['Pages'][i]['Texts'][k]['R'][m]['T']);
+                        magicIndex[0] = i;
+                        magicIndex[1] = k;
+                        magicIndex[2] = m;
+                    }  
+                }
+            }
+        }
+    }
+    console.log(magicIndex);
+    console.log(data['Pages'][magicIndex[0]]['Texts'][magicIndex[1] + 20]['R'][magicIndex[2]]['T']);
+    res.status(200).send(data);
 })
 
 const pdfParser = new PDFParser();
@@ -18,23 +42,11 @@ pdfParser.on('pdfParser_dataError', err => console.error(err.parserError));
 pdfParser.on('pdfParser_dataReady', data => {
     // console.log(data);
     // console.log(data['Pages'][0]['Texts'][0]['R'][0]['T']);
-    // fs.writeFile("test.json", JSON.stringify(pdfData));
-    // for (let i = 0; i < data['Pages'].length; i++) {
-    //     console.log(i)
-    //     console.log(data['Pages'][i]['Texts'].length);
-    //     console.log(data['Pages'][i]['Texts'][0]['R'][0]['T']);
-        // for (let k = 0; k < data['Pages'][i]['Texts'].length; k++) {
-        //     if (i == data['Pages'].length) {
-        //         console.log(k);
-        //     }
-        // }
-    // }
-    // fs.writeFile('test.json', data, () => {
-
-    // });
+    
+    // fs.writeFile('test.json', data, () => {});
 });
 
-// pdfParser.loadPDF('test.pdf');
+
 
 fs.readFile('test.json', (data, err) => {
     console.log(data);
