beb0b51f910c81e477e434f34ace06c9e6dab6fd
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-10-26 07:36:09 +0200

    Changed to ' instead of "

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Imports
 const fs = require('fs'),
-PDFParser = require("pdf2json");
+PDFParser = require('pdf2json');
 
 const pdfParser = new PDFParser();
 
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-pdfParser.on("pdfParser_dataReady", pdfData => {
-    console.log(JSON.stringify(pdfData));
-    fs.writeFile("test.json", JSON.stringify(pdfData));
+pdfParser.on('pdfParser_dataError', err => console.error(err.parserError));
+pdfParser.on('pdfParser_dataReady', data => {
+    console.log(data);
+    console.log(data['Pages'][0]);
+    // fs.writeFile("test.json", JSON.stringify(pdfData));
 });
 
 pdfParser.loadPDF("test.pdf");
