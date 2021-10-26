a1194d837eeea2e2894da4db175e75751a0873a0
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-10-26 07:39:36 +0200

    Found the way to extract text fields.

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const pdfParser = new PDFParser();
 
 pdfParser.on('pdfParser_dataError', err => console.error(err.parserError));
 pdfParser.on('pdfParser_dataReady', data => {
-    console.log(data);
-    console.log(data['Pages'][0]);
+    // console.log(data);
+    console.log(data['Pages'][0]['Texts'][0]['R'][0]['T']);
     // fs.writeFile("test.json", JSON.stringify(pdfData));
 });
 
-pdfParser.loadPDF("test.pdf");
+pdfParser.loadPDF('test.pdf');
 
 
